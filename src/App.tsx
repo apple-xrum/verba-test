5c9f02fe9abb9bc000e163144de67a9bfc8c2d4a
AppleXrum <156526897+App1eXrum@users.noreply.github.com>
2024-11-02 03:21:43 +0300

    Настроены пути приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import TodoList from './components/TodoList';
 
 function App() {
+  //const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  const isLoggedIn = true;
+
   return (
-    <>
-      <h1>Verba To-Do List</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <TodoList /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/all"
+          element={isLoggedIn ? <TodoList /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/completed"
+          element={isLoggedIn ? <TodoList /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/deleted"
+          element={isLoggedIn ? <TodoList /> : <Navigate to="/signin" />}
+        />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList: React.FC = () => {
+  return (
+    <div>
+      TodoList
+    </div>
+  );
+};
+
+export default TodoList;
