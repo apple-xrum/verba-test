d4d61461791767ec80c579963f9b7fae07044f11
AppleXrum <156526897+App1eXrum@users.noreply.github.com>
2024-11-02 04:32:08 +0300

    Добавлено меню смены типа тудушек

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
+  const location = useLocation();
+
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <ul>
+        <li className={location.pathname === '/' ? 'active' : ''}>
+          <Link to="/">
+            Текущие дела
+          </Link>
+        </li>
+        <li className={location.pathname === '/all' ? 'active' : ''}>
+          <Link to="/all">
+            Все дела
+          </Link>
+        </li>
+        <li className={location.pathname === '/completed' ? 'active' : ''}>
+          <Link to="/completed">
+            Выполненные дела
+          </Link>
+        </li>
+        <li className={location.pathname === '/deleted' ? 'active' : ''}>
+          <Link to="/deleted">
+            Корзина
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const TodoList: React.FC = () => {
   return (
     <div>
+      <Navbar />
       TodoList
     </div>
   );
